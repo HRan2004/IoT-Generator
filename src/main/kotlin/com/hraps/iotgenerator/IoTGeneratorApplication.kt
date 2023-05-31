ab071d0650b1a265980ae595736dc489e4347019
HRan2004 <2125764918@qq.com>
2023-06-01 00:41:12 +0800

    feat: 初始搭建Spring项目

---
+++ src/main/kotlin/com/hraps/iotgenerator/IoTGeneratorApplication.kt
@@ -0,0 +1,11 @@
+package com.hraps.iotgenerator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IoTGeneratorApplication
+
+fun main(args: Array<String>) {
+    runApplication<IoTGeneratorApplication>(*args)
+}
---
+++ src/test/kotlin/com/hraps/iotgenerator/IoTGeneratorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hraps.iotgenerator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IoTGeneratorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
